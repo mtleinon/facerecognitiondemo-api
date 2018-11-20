df32d9b59d9d96a68936eba6e6e476f409255ee6
mtleinon <mika.t.leinonen@gmail.com>
2018-11-20 19:22:03 +0200

    Modified Heroku Postgres database configuration part 2.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const profile = require("./controllers/profile");
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true,
         //host: '127.0.0.1',
         // user: 'postgres',
